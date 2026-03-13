26c78ad967016ab55b7cd8f6b27341ca8bd75d03
Yl John Flores M. <Smoshiee34@gmail.com>
2026-03-13 23:50:45 +0800

    Update ContentSecurityPolicy.php

--- app/Config/ContentSecurityPolicy.php
+++ app/Config/ContentSecurityPolicy.php
@@ -2,176 +2,36 @@
 
 namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\HTTP\ContentSecurityPolicy\Config;
 
-/**
- * Stores the default settings for the ContentSecurityPolicy, if you
- * choose to use it. The values here will be read in and set as defaults
- * for the site. If needed, they can be overridden on a page-by-page basis.
- *
- * Suggested reference for explanations:
- *
- * @see https://www.html5rocks.com/en/tutorials/security/content-security-policy/
- */
-class ContentSecurityPolicy extends BaseConfig
+class ContentSecurityPolicy extends Config
 {
-    // -------------------------------------------------------------------------
-    // Broadbrush CSP management
-    // -------------------------------------------------------------------------
+    /**
+     * Default policy directives for your app
+     */
+    public array $defaultDirectives = [
+        'default-src' => "'self'",
+        'script-src'  => [
+            "'self'",
+            // Add trusted script sources here (e.g., for analytics or widgets)
+        ],
+        'style-src'   => [
+            "'self'",
+            "'unsafe-inline'", // Temporary if you use inline styles; remove when possible
+        ],
+        'img-src'     => [
+            "'self'",
+            'data:', // Allow inline images (e.g., base64) if needed
+        ],
+        'connect-src' => "'self'",
+        'font-src'    => "'self'",
+        'object-src'  => "'none'",
+        'frame-src'   => "'none'",
+        'report-uri'  => '/csp-report', // Optional: For violation reports
+    ];
 
     /**
-     * Default CSP report context
+     * Whether to send the policy as "report-only" (no blocking, just reports)
      */
     public bool $reportOnly = false;
-
-    /**
-     * Specifies a URL where a browser will send reports
-     * when a content security policy is violated.
-     */
-    public ?string $reportURI = null;
-
-    /**
-     * Instructs user agents to rewrite URL schemes, changing
-     * HTTP to HTTPS. This directive is for websites with
-     * large numbers of old URLs that need to be rewritten.
-     */
-    public bool $upgradeInsecureRequests = false;
-
-    // -------------------------------------------------------------------------
-    // Sources allowed
-    // NOTE: once you set a policy to 'none', it cannot be further restricted
-    // -------------------------------------------------------------------------
-
-    /**
-     * Will default to self if not overridden
-     *
-     * @var list<string>|string|null
-     */
-    public $defaultSrc;
-
-    /**
-     * Lists allowed scripts' URLs.
-     *
-     * @var list<string>|string
-     */
-     public $scriptSrc = ['self', "'unsafe-inline'", "'unsafe-eval'"];
-
-
-    /**
-     * Lists allowed stylesheets' URLs.
-     *
-     * @var list<string>|string
-     */
-    public $styleSrc = ['self', "'unsafe-inline'"];
-
-    /**
-     * Defines the origins from which images can be loaded.
-     *
-     * @var list<string>|string
-     */
-    public $imageSrc = 'self';
-
-    /**
-     * Restricts the URLs that can appear in a page's `<base>` element.
-     *
-     * Will default to self if not overridden
-     *
-     * @var list<string>|string|null
-     */
-    public $baseURI;
-
-    /**
-     * Lists the URLs for workers and embedded frame contents
-     *
-     * @var list<string>|string
-     */
-    public $childSrc = 'self';
-
-    /**
-     * Limits the origins that you can connect to (via XHR,
-     * WebSockets, and EventSource).
-     *
-     * @var list<string>|string
-     */
-    public $connectSrc = 'self';
-
-    /**
-     * Specifies the origins that can serve web fonts.
-     *
-     * @var list<string>|string
-     */
-    public $fontSrc;
-
-    /**
-     * Lists valid endpoints for submission from `<form>` tags.
-     *
-     * @var list<string>|string
-     */
-    public $formAction = 'self';
-
-    /**
-     * Specifies the sources that can embed the current page.
-     * This directive applies to `<frame>`, `<iframe>`, `<embed>`,
-     * and `<applet>` tags. This directive can't be used in
-     * `<meta>` tags and applies only to non-HTML resources.
-     *
-     * @var list<string>|string|null
-     */
-    public $frameAncestors;
-
-    /**
-     * The frame-src directive restricts the URLs which may
-     * be loaded into nested browsing contexts.
-     *
-     * @var list<string>|string|null
-     */
-    public $frameSrc;
-
-    /**
-     * Restricts the origins allowed to deliver video and audio.
-     *
-     * @var list<string>|string|null
-     */
-    public $mediaSrc;
-
-    /**
-     * Allows control over Flash and other plugins.
-     *
-     * @var list<string>|string
-     */
-    public $objectSrc = 'self';
-
-    /**
-     * @var list<string>|string|null
-     */
-    public $manifestSrc;
-
-    /**
-     * Limits the kinds of plugins a page may invoke.
-     *
-     * @var list<string>|string|null
-     */
-    public $pluginTypes;
-
-    /**
-     * List of actions allowed.
-     *
-     * @var list<string>|string|null
-     */
-    public $sandbox;
-
-    /**
-     * Nonce tag for style
-     */
-    public string $styleNonceTag = '{csp-style-nonce}';
-
-    /**
-     * Nonce tag for script
-     */
-    public string $scriptNonceTag = '{csp-script-nonce}';
-
-    /**
-     * Replace nonce tag automatically
-     */
-    public bool $autoNonce = true;
 }
