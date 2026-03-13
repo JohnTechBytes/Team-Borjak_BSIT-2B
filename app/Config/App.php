cc30c4c451a43b1e9ea8f8391696894cc7845b08
Yl John Flores M. <Smoshiee34@gmail.com>
2026-03-13 23:49:45 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -10,26 +10,16 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Base Site URL
      * --------------------------------------------------------------------------
-     *
-     * URL to your CodeIgniter root. Typically, this will be your base URL,
-     * WITH a trailing slash:
-     *
-     * E.g., http://example.com/
+     * URL to your CodeIgniter root (use your actual Vercel app URL with HTTPS)
      */
-    // public string $baseURL = 'http://localhost:8080/';
-
-    public $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://team-borjak-bsit-2b.vercel.app/';
 
     /**
-     * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
-     * If you want to accept multiple Hostnames, set this.
-     *
-     * E.g.,
-     * When your site URL ($baseURL) is 'http://example.com/', and your site
-     * also accepts 'http://media.example.com/' and 'http://accounts.example.com/':
-     *     ['media.example.com', 'accounts.example.com']
-     *
-     * @var list<string>
+     * --------------------------------------------------------------------------
+     * Allowed Hostnames
+     * --------------------------------------------------------------------------
+     * Hostnames other than baseURL that your app should accept.
+     * NOTE: http://media.example.com/ and http://accounts.example.com/ return errors (per docs), so left empty for now.
      */
     public array $allowedHostnames = [];
 
@@ -37,65 +27,31 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Index File
      * --------------------------------------------------------------------------
-     *
-     * Typically, this will be your `index.php` file, unless you've renamed it to
-     * something else. If you have configured your web server to remove this file
-     * from your site URIs, set this variable to an empty string.
+     * Empty string removes index.php from URLs (configured via vercel.json/.htaccess)
      */
-    // public string $indexPage = 'index.php';
-
     public string $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
      * URI PROTOCOL
      * --------------------------------------------------------------------------
-     *
-     * This item determines which server global should be used to retrieve the
-     * URI string. The default setting of 'REQUEST_URI' works for most servers.
-     * If your links do not seem to work, try one of the other delicious flavors:
-     *
-     *  'REQUEST_URI': Uses $_SERVER['REQUEST_URI']
-     * 'QUERY_STRING': Uses $_SERVER['QUERY_STRING']
-     *    'PATH_INFO': Uses $_SERVER['PATH_INFO']
-     *
-     * WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
+     * Default setting works for most servers (including Vercel)
      */
     public string $uriProtocol = 'REQUEST_URI';
 
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed URL Characters
-    |--------------------------------------------------------------------------
-    |
-    | This lets you specify which characters are permitted within your URLs.
-    | When someone tries to submit a URL with disallowed characters they will
-    | get a warning message.
-    |
-    | As a security measure you are STRONGLY encouraged to restrict URLs to
-    | as few characters as possible.
-    |
-    | By default, only these are allowed: `a-z 0-9~%.:_-`
-    |
-    | Set an empty string to allow all characters -- but only if you are insane.
-    |
-    | The configured value is actually a regular expression character group
-    | and it will be used as: '/\A[<permittedURIChars>]+\z/iu'
-    |
-    | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
-    |
-    */
+    /**
+     * --------------------------------------------------------------------------
+     * Allowed URL Characters
+     * --------------------------------------------------------------------------
+     * Restrict to safe characters (do not change unless absolutely necessary)
+     */
     public string $permittedURIChars = 'a-z 0-9~%.:_\-';
 
     /**
      * --------------------------------------------------------------------------
      * Default Locale
      * --------------------------------------------------------------------------
-     *
-     * The Locale roughly represents the language and location that your visitor
-     * is viewing the site from. It affects the language strings and other
-     * strings (like currency markers, numbers, etc), that your program
-     * should run under for this request.
+     * Set to 'fil' for Filipino if needed; 'en' is default
      */
     public string $defaultLocale = 'en';
 
@@ -103,11 +59,7 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Negotiate Locale
      * --------------------------------------------------------------------------
-     *
-     * If true, the current Request object will automatically determine the
-     * language to use based on the value of the Accept-Language header.
-     *
-     * If false, no automatic detection will be performed.
+     * Disable unless you need automatic language detection from user headers
      */
     public bool $negotiateLocale = false;
 
@@ -115,14 +67,7 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Supported Locales
      * --------------------------------------------------------------------------
-     *
-     * If $negotiateLocale is true, this array lists the locales supported
-     * by the application in descending order of priority. If no match is
-     * found, the first locale will be used.
-     *
-     * IncomingRequest::setLocale() also uses this list.
-     *
-     * @var list<string>
+     * Add 'fil' to support Filipino language strings
      */
     public array $supportedLocales = ['en'];
 
@@ -130,24 +75,15 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Application Timezone
      * --------------------------------------------------------------------------
-     *
-     * The default timezone that will be used in your application to display
-     * dates with the date helper, and can be retrieved through app_timezone()
-     *
-     * @see https://www.php.net/manual/en/timezones.php for list of timezones
-     *      supported by PHP.
+     * Correct timezone for Cebu/Philippines (per PHP docs)
      */
-    public string $appTimezone = 'UTC';
+    public string $appTimezone = 'Asia/Manila';
 
     /**
      * --------------------------------------------------------------------------
      * Default Character Set
      * --------------------------------------------------------------------------
-     *
-     * This determines which character set is used by default in various methods
-     * that require a character set to be provided.
-     *
-     * @see http://php.net/htmlspecialchars for a list of supported charsets.
+     * UTF-8 is standard and compatible with htmlspecialchars() (per docs)
      */
     public string $charset = 'UTF-8';
 
@@ -155,34 +91,15 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Force Global Secure Requests
      * --------------------------------------------------------------------------
-     *
-     * If true, this will force every request made to this application to be
-     * made via a secure connection (HTTPS). If the incoming request is not
-     * secure, the user will be redirected to a secure version of the page
-     * and the HTTP Strict Transport Security (HSTS) header will be set.
+     * Enforce HTTPS (required for Vercel and secure production deployments)
      */
-    public bool $forceGlobalSecureRequests = false;
+    public bool $forceGlobalSecureRequests = true;
 
     /**
      * --------------------------------------------------------------------------
      * Reverse Proxy IPs
      * --------------------------------------------------------------------------
-     *
-     * If your server is behind a reverse proxy, you must whitelist the proxy
-     * IP addresses from which CodeIgniter should trust headers such as
-     * X-Forwarded-For or Client-IP in order to properly identify
-     * the visitor's IP address.
-     *
-     * You need to set a proxy IP address or IP address with subnets and
-     * the HTTP header for the client IP address.
-     *
-     * Here are some examples:
-     *     [
-     *         '10.0.1.200'     => 'X-Forwarded-For',
-     *         '192.168.5.0/24' => 'X-Real-IP',
-     *     ]
-     *
-     * @var array<string, string>
+     * Vercel uses reverse proxies; add trusted IP ranges if needed (optional)
      */
     public array $proxyIPs = [];
 
@@ -190,19 +107,15 @@ class App extends BaseConfig
      * --------------------------------------------------------------------------
      * Content Security Policy
      * --------------------------------------------------------------------------
-     *
-     * Enables the Response's Content Secure Policy to restrict the sources that
-     * can be used for images, scripts, CSS files, audio, video, etc. If enabled,
-     * the Response object will populate default values for the policy from the
-     * `ContentSecurityPolicy.php` file. Controllers can always add to those
-     * restrictions at run time.
-     *
-     * For a better understanding of CSP, see these documents:
-     *
-     * @see http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-     * @see http://www.w3.org/TR/CSP/
+     * Enable to prevent XSS attacks (configure rules in ContentSecurityPolicy.php)
      */
-    public bool $CSPEnabled = false;
-        // Enable CSRF protection
-    public $CSRFProtection = true;
+    public bool $CSPEnabled = true;
+
+    /**
+     * --------------------------------------------------------------------------
+     * CSRF Protection
+     * --------------------------------------------------------------------------
+     * Enable to prevent cross-site request forgery attacks
+     */
+    public bool $CSRFProtection = true;
 }
